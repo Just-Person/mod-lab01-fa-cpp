ce7a442c91f8f39bd5f27bfec70ed317ebd75336
Ilya <40660169+Just-Person@users.noreply.github.com>
2022-03-11 01:44:22 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include "fun.h"
-int main()
-{
-	std::cout << faStr1("gra f4f2 32 fda dsafd")<<"\n";
-	std::cout << faStr2("A Aa Adsad   AA 31ASD aAds AadA  aaaa aaaA")<<"\n";
-	std::cout << faStr3("A Aa Adsad   AA 31ASD aAds AadAaaaaaaaa  aaaa aaaA");
-	return 1;
+int main() {
+ std::cout << faStr1("gra f4f2 32 fda dsafd")<<"\n";
+ std::cout << faStr2("A Aa Adsad   AA 31ASD aAds AadA  aaaa aaaA")<<"\n";
+ std::cout << faStr3("A Aa Adsad   AA 31ASD aAds AadAaaaaaaaa  aaaa aaaA");
+ return 1;
 }
