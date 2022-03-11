2968d825ec0a0523346c54535731746cea2605c6
Ilya <40660169+Just-Person@users.noreply.github.com>
2022-03-12 01:12:07 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include "fun.h"
 int main() {
 std::cout << faStr1("gra f4f2 32 fda dsafd") << "\n";
-std::cout << faStr2("A Aa Adsad   AA 31ASD aAds AadA  aaaa aaaA")<<"\n";
+std::cout << faStr2("A Aa Adsad   AA 31ASD aAds AadA  aaaa aaaA") << "\n";
 std::cout << faStr3("A Aa Adsad   AA 31ASD aAds AadAaaaaaaaa  aaaa aaaA");
 return 1;
 }
