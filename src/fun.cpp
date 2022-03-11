70253ffc01a42748ed2dffc4b3aed291d2533460
Ilya <40660169+Just-Person@users.noreply.github.com>
2022-03-12 01:27:17 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,7 +1,7 @@
 // Copyright 2022 UNN-IASR
 #include <iostream>
-#include <cctype>
 #include <math.h>
+#include <cctype>
 #include "fun.h"
 
 unsigned int faStr1(const char* str) {
@@ -13,15 +13,13 @@ if (*(str + iterator) != ' '
 && !isdigit(*(str + iterator)) && !finWord(inWord)) {
 count++;
 inWord = 1;
-}
-else if (*(str + iterator) == ' ' && finWord(inWord))
+} else if (*(str + iterator) == ' ' && finWord(inWord))
 inWord = 0;
 else if (isdigit(*(str + iterator)) && inWord == 0)
 inWord = 2;
 else if (isdigit(*(str + iterator)) && inWord == 1) {
 inWord = 2;
-count--;
-}
+count--;}
 iterator++;
 }
 return count;
@@ -34,16 +32,14 @@ while (*(str + iterator)) {
 if (isupper(*(str + iterator)) && inWord == 0) {
 count++;
 inWord = 1;
-}
-else if (!islowermy(*(str + iterator))
+} else if (!islowermy(*(str + iterator))
 && !finWord(inWord))
 inWord = 2;
 else if (*(str + iterator) == ' ' && finWord(inWord))
 inWord = 0;
 else if (!islowermy(*(str + iterator)) && inWord == 1) {
 inWord = 2;
-count--;
-}
+count--;}
 iterator++;
 }
 return count;
@@ -57,10 +53,8 @@ while (*(str + iterator)) {
 if (*(str + iterator) != ' ' && inWord == false) {
 count++;
 inWord = true;
-}
-else if (*(str + iterator) == ' ' && inWord == true) {
-inWord = false;
-}
+} else if (*(str + iterator) == ' ' && inWord == true) {
+inWord = false;}
 if (inWord == true)
 sum++;
 iterator++;
